adbec3c9a817dac72f49e7d52cda811dd0168d26
shehzad-hash <shehzadkhan1564008@gmail.com>
2023-01-26 01:56:30 +0500

    dashboard update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 function sideMenuToggle(){
     document.querySelector(".sideBar").classList.toggle("sideBarShow")
 }
+
+// tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
