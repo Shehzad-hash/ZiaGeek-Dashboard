855a995b69b801dfbd9b184a2285e5aa97431c8d
shehzad-hash <shehzadkhan1564008@gmail.com>
2023-01-27 23:57:11 +0500

    dashboard update

--- js/main.js
+++ js/main.js
@@ -5,3 +5,48 @@ function sideMenuToggle(){
 // tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// change value
+let value=document.getElementById("value")
+let value2=document.getElementById("value2")
+let result
+function leftChange(){
+    result=value.innerText
+    result=result.trim()
+    if(result==="50,000"){
+        value.innerText="25,000"
+    }
+    if(result==="100,000"){
+        value.innerText="50,000"
+    }
+}
+function rightChange(){
+    result=value.innerText
+    result=result.trim()
+    if(result==="25,000"){
+        value.innerText="50,000"
+    }
+    if(result==="50,000"){
+        value.innerText="100,000"
+    }
+}
+function leftChange2(){
+    result=value2.innerText
+    result=result.trim()
+    if(result==="50,000"){
+        value2.innerText="25,000"
+    }
+    if(result==="100,000"){
+        value2.innerText="50,000"
+    }
+}
+function rightChange2(){
+    result=value2.innerText
+    result=result.trim()
+    if(result==="25,000"){
+        value2.innerText="50,000"
+    }
+    if(result==="50,000"){
+        value2.innerText="100,000"
+    }
+}
